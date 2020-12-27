c9d004e8add9aa92e1b783d5a619dc6614e86a0b
Richard Morey <richarddmorey@gmail.com>
2020-12-27 19:10:29 +0000

    prepared github pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import PowerInterface from './components/PowerInterface'
-
-Vue.component('powerInterface', PowerInterface)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app'
-})
